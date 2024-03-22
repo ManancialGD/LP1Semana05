c807f8c65fbcb1e013be9d8ed0462e38ee1cd11f
“[Breno <manancialgd@gmail.com>
2024-03-22 17:55:23 +0000

    RanDice Is working

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -4,9 +4,28 @@ namespace RanDice
 {
     class Program
     {
+        static int n;
+        static int s;
+        static int sum;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            n = int.Parse(args[0]);
+            s = int.Parse(args[1]);
+
+            sum = SumDice(n, s);
+            Console.WriteLine(sum);
+        }
+
+        static int SumDice(int numberOfDice, int seed)
+        {
+            int tmpSum = 0;
+            Random r = new Random(seed);
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                tmpSum += r.Next(1,7);
+                Console.WriteLine(tmpSum);
+            }
+            return tmpSum;
         }
     }
 }
